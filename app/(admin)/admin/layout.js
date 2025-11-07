45ca337b64beb9b670ffe3b9c96498cda813d02a
Sabin Magar <alemagarsabin123@gmail.com>
2025-11-07 21:33:04 +0545

    Force dynamic rendering for admin routes

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -4,6 +4,8 @@ import { notFound } from 'next/navigation';
 import React from 'react'
 import Sidebar from './_components/sidebar';
 
+export const dynamic = 'force-dynamic';
+
 const AdminLayout = async ({ children }) => {
 
   const admin = await getAdmin();
